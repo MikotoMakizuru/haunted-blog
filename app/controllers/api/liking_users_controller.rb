6097b1c0da4c9274b40ffae74eecdca9eb85fb3e
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-06-16 20:37:05 +0900

    Jbuilderがあったことに気づく

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -6,7 +6,5 @@ class Api::LikingUsersController < ApplicationController
     @users = blog.liking_users.order(:id)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
-
-    render json: { users: @users.as_json(only: %i[nickname]), destroy_path: @destroy_path }
   end
 end
